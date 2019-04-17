9a3831fda11517e46e8604c469e7d2765737795c
Peter Krautzberger <p.krautzberger@gmail.com>
2019-04-17 11:27:09 +0200

    chore: get ready for test release

--- mathtools.js
+++ mathtools.js
@@ -18,159 +18,18 @@ MathJax.Extension['TeX/mathtools'] = {
   version: '1.0.0'
 };
 
-MathJax.Hub.Register.StartupHook('TeX Jax Ready', function() {
+MathJax.Hub.Register.StartupHook('TeX AMSmath Ready', function() {
   var TEX = MathJax.InputJax.TeX;
   TEX.Definitions.Add({
     macros: {
-      coloneqq: ['Macro', '\\mathrel{≔}'],
+      coloneqq: ['Macro', '\\mathrel{≔}']
     },
     environment: {
       'bmatrix*': ['Array', 'bmatrix*', '[', ']'],
       dcases: ['Array', null, '\\{', '.', 'll', null, '.2em', 'D'],
-      bsmallmatrix: ['Array', null, '[', ']', 'c', '0.333em', '.2em', 'S', 1],
-      multlined: ['Array', null, '[', ']', 'c', '0.333em', '.2em', 'S', 1]
+      bsmallmatrix: ['Array', null, '[', ']', 'c', '0.333em', '.2em', 'S', 1]
     }
   });
-});
-
-MathJax.Hub.Register.StartupHook('TeX AMSmath Ready', function() {
-  var MML = MathJax.ElementJax.mml,
-    TEX = MathJax.InputJax.TeX,
-    TEXDEF = TEX.Definitions,
-    STACKITEM = TEX.Stack.Item;
-
-  TEXDEF.Add({
-    environment: {
-      mathtoolsat: ['mathtoolsAt', null, true, true],
-      'mathtoolsat*': ['mathtoolsAt', null, false, true],
-      xmathtoolsat: ['mathtoolsAt', null, true, false],
-      'xmathtoolsat*': ['mathtoolsAt', null, false, false],
-      flalign: [
-        'mathtoolsArray',
-        null,
-        true,
-        false,
-        true,
-        'rlcrlcrlcrlcrlcrlc',
-        ['', ' ', ' ', ' ', ' ', ' ', ''].join('auto auto fit')
-      ],
-      'flalign*': [
-        'mathtoolsArray',
-        null,
-        false,
-        false,
-        true,
-        'rlcrlcrlcrlcrlcrlc',
-        ['', ' ', ' ', ' ', ' ', ' ', ''].join('auto auto fit')
-      ]
-    }
-  });
-
-  TEX.Parse.Augment({
-    mathtoolsAt: function(begin, numbered, padded) {
-      var n,
-        align = [],
-        width = [];
-      n = this.GetArgument('\\begin{' + begin.name + '}');
-      if (n.match(/[^0-9]/)) {
-        TEX.Error([
-          'PositiveIntegerArg',
-          'Argument to %1 must me a positive integer',
-          '\\begin{' + begin.name + '}'
-        ]);
-      }
-      if (padded) {
-        align.push('');
-        width.push('');
-      }
-      while (n > 0) {
-        align.push('rl');
-        width.push('auto auto');
-        n--;
-      }
-      if (padded) {
-        align.push('');
-        width.push('');
-      }
-      return this.mathtoolsArray(
-        begin,
-        numbered,
-        padded,
-        false,
-        align.join('c'),
-        width.join(' fit ')
-      );
-    },
-
-    mathtoolsArray: function(begin, numbered, padded, center, align, width) {
-      this.Push(begin);
-      this.checkEqnEnv();
-      align = align
-        .split('')
-        .join(' ')
-        .replace(/r/g, 'right')
-        .replace(/l/g, 'left')
-        .replace(/c/g, 'center');
-      return STACKITEM.mathtoolsArray(
-        begin.name,
-        numbered,
-        padded,
-        center,
-        this.stack
-      ).With({
-        arraydef: {
-          width: '100%',
-          displaystyle: true,
-          columnalign: align,
-          columnspacing: '0em',
-          columnwidth: width,
-          rowspacing: '3pt',
-          side: TEX.config.TagSide,
-          minlabelspacing: TEX.config.TagIndent
-        }
-      });
-    }
-  });
-
-  STACKITEM.mathtoolsArray = STACKITEM.AMSarray.Subclass({
-    type: 'mathtoolsArray',
-    Init: function(name, numbered, padded, center, stack) {
-      this.SUPER(arguments).Init(name, numbered, padded || center, stack);
-      this.padded = padded;
-      this.center = center;
-      this.maxrow = 0;
-    },
-    EndRow: function() {
-      var cell,
-        row = this.row;
-      this.row = [];
-      if (this.padded) this.row.push(MML.mtd());
-      while ((cell = row.shift())) {
-        this.row.push(cell);
-        cell = row.shift();
-        if (cell) this.row.push(cell);
-        if (row.length || this.padded) this.row.push(MML.mtd());
-      }
-      if (this.row.length > this.maxrow) this.maxrow = this.row.length;
-      this.SUPER(arguments).EndRow.call(this);
-    },
-    EndTable: function() {
-      this.SUPER(arguments).EndTable.call(this);
-      if (this.center) {
-        var def = this.arraydef;
-        if (this.maxrow <= 2) delete def.width;
-        def.columnalign = def.columnalign
-          .split(/ /)
-          .slice(0, this.maxrow)
-          .join(' ');
-        def.columnwidth = def.columnwidth
-          .split(/ /)
-          .slice(0, this.maxrow)
-          .join(' ');
-      }
-    }
-  });
-
   MathJax.Hub.Startup.signal.Post('TeX mathtools Ready');
 });
 
